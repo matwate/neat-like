cf811de0a23d870f33199e435ee283e0a5571dda
matwa <mateowatata@gmail.com>
2024-12-03 12:46:43 -0500

    Deleted everything for a fresh start

--- v3/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/matwate/neat-like/v3
-
-go 1.23.3
--- v3/node.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// Structural node
-type Node struct {
-	activation activationFun // activation function
-	outgoing   []int         // outgoing edges
-	incoming   int           // Amount of incoming edges
-	depth      int           //
-}
